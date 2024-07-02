95a3d009b6b21bff76b2d3e7863f928388abf6b4
eotel <49769982+Eotel@users.noreply.github.com>
2024-07-02 22:23:12 +0900

    🎨 .editorconfig に tab indent の設定があるのでフォーマット

--- shapesns/bootstrap.php
+++ shapesns/bootstrap.php
@@ -2,42 +2,42 @@
 
 class ClassLoader
 {
-    public static function loadClass($class)
-    {
-        foreach (self::directories() as $directory) {
-            $file_name = str_replace(
-                "\\",
-                "/",
-                $directory . DIRECTORY_SEPARATOR . $class . ".php"
-            );
+	public static function loadClass($class)
+	{
+		foreach (self::directories() as $directory) {
+			$file_name = str_replace(
+				"\\",
+				"/",
+				$directory . DIRECTORY_SEPARATOR . $class . ".php"
+			);
 
-            if (is_file($file_name)) {
-                require $file_name;
-                return true;
-            }
-        }
-    }
+			if (is_file($file_name)) {
+				require $file_name;
+				return true;
+			}
+		}
+	}
 
-    private static $dirs;
+	private static $dirs;
 
-    private static function directories()
-    {
-        if (empty(self::$dirs)) {
-            $base = __DIR__;
-            self::$dirs = array(
-                $base,
-            );
-        }
+	private static function directories()
+	{
+		if (empty(self::$dirs)) {
+			$base = __DIR__;
+			self::$dirs = array(
+				$base,
+			);
+		}
 
-        return self::$dirs;
-    }
+		return self::$dirs;
+	}
 
-    public static function app_instance($appclass_name)
-    {
-        require_once __DIR__ . '/vendor/autoload.php';
-        Timber\Timber::init();
+	public static function app_instance($appclass_name)
+	{
+		require_once __DIR__ . '/vendor/autoload.php';
+		Timber\Timber::init();
 
-        spl_autoload_register(array('ClassLoader', 'loadClass'));
-        return call_user_func(array($appclass_name, 'get_instance'));
-    }
+		spl_autoload_register(array('ClassLoader', 'loadClass'));
+		return call_user_func(array($appclass_name, 'get_instance'));
+	}
 }
