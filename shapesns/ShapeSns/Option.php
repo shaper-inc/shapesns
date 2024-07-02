79ac78149c57af1bb8a68d935d6afec7ee53fbfa
user.name <gmail@hdknr.com>
2024-07-02 13:12:30 +0900

    Option: wp_option テーブルへのアクセサ

---
+++ shapesns/ShapeSns/Option.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ShapeSns;
+
+class Option extends Base
+{
+    protected $key = "ShapSns";
+    protected $option = array(
+        'chat_gpt_key' => '',
+    );
+
+    function __construct(array $argument = array())
+    {
+        $opt = get_option($this->key, array());
+        if ($opt != null) {
+            $this->option = $opt;
+        }
+    }
+
+    function __get($name)
+    {
+        if (array_key_exists($name, $this->option)) {
+            return $this->option[$name];
+        }
+    }
+
+    function __set($name, $value)
+    {
+        error_log("__set: $name = $value ");
+        if (array_key_exists($name, $this->option)) {
+            $this->option[$name] = $value;
+        } else {
+            error_log("__set: $name not found");
+        }
+    }
+
+    function get()
+    {
+        return $this->option;
+    }
+
+    function update($values = null)
+    {
+        if ($values != null) {
+            foreach ($values as $k => $v) {
+                $this->option[$k] = $v;
+            }
+        }
+        update_option($this->key, $this->option);
+    }
+}
