1cf214a46f48cd112b1ee2b986fff666ad4bd424
eotel <49769982+Eotel@users.noreply.github.com>
2024-07-02 22:25:43 +0900

    🩹 bracket が閉じていない問題修正

--- shapesns/ShapeSns/App.php
+++ shapesns/ShapeSns/App.php
@@ -4,43 +4,44 @@ namespace ShapeSns;
 
 class App extends AppBase
 {
-    protected $admin = null;
-    protected $option = null;
+	protected $admin = null;
+	protected $option = null;
 
-    function __construct(array $argument = array())
-    {
-        parent::__construct($argument);
-        $this->admin = Admin::get_instance();
-        $this->option = Option::get_instance();
-    }
+	function __construct(array $argument = array())
+	{
+		parent::__construct($argument);
+		$this->admin = Admin::get_instance();
+		$this->option = Option::get_instance();
+	}
 
-    # 管理画面
-    function action_admin_menu()
-    {
-        add_options_page(
-            'ShapeSns options',
-            'ShapeSns',
-            'manage_options',
-            'ShapeSns',
-            array($this->admin, 'options_page')
-        );
-    }
+	# 管理画面
+	function action_admin_menu()
+	{
+		add_options_page(
+			'ShapeSns options',
+			'ShapeSns',
+			'manage_options',
+			'ShapeSns',
+			array($this->admin, 'options_page')
+		);
+	}
 
 
-    function summarize_post($post_id)
-    {
-        # post_id の記事の要約を取得する
-        echo  $post_id;
+	function summarize_post($post_id)
+	{
+		# post_id の記事の要約を取得する
+		echo $post_id;
+	}
 
-    function action_wp_enqueue_scripts()
-    {
-        # Javvascriptを配信
-        # https://developer.wordpress.org/reference/functions/wp_enqueue_script/
-        # https://developer.wordpress.org/reference/functions/plugins_url/
-        wp_enqueue_script('shapesns', plugins_url('/js/shapesns.js', dirname(__FILE__)));
+	function action_wp_enqueue_scripts()
+	{
+		# Javvascriptを配信
+		# https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+		# https://developer.wordpress.org/reference/functions/plugins_url/
+		wp_enqueue_script('shapesns', plugins_url('/js/shapesns.js', dirname(__FILE__)));
+	}
 
-    }
-
-    # TOOD:
-    # wordpress hook を実装する
+	# TOOD:
+	# wordpress hook を実装する
 }
+
