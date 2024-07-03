6e7603ae88e5d020ce3068496fbb164b49e57f42
user.name <gmail@hdknr.com>
2024-07-03 11:36:51 +0900

    <head>に meta で 記事要約を差し込む

--- shapesns/ShapeSns/App.php
+++ shapesns/ShapeSns/App.php
@@ -6,6 +6,7 @@ class App extends AppBase
 {
     protected $admin = null;
     protected $option = null;
+    protected $meta_key = "記事要約";   // TODO 設定
 
     function __construct(array $argument = array())
     {
@@ -35,14 +36,13 @@ class App extends AppBase
     {
 
         $wp_post = get_post($post_id);
-        $meta_key = "記事要約";
 
         if ($wp_post) {
             $content = $wp_post->post_content;
             $title = $wp_post->post_title;
             $summary = $this->summarize_text($content);
             if ($summary) {
-                update_post_meta($post_id, $meta_key, $summary);
+                update_post_meta($post_id, $this->meta_key, $summary);
             }
         }
     }
@@ -53,6 +53,23 @@ class App extends AppBase
         # https://developer.wordpress.org/reference/functions/wp_enqueue_script/
         # https://developer.wordpress.org/reference/functions/plugins_url/
         wp_enqueue_script('shapesns', plugins_url('/js/shapesns.js', dirname(__FILE__)));
+    }
+
+    function action_wp_head()
+    {
+        # ヘッダーに要約を入れる
+        $post = get_post();
+        if (!$post) {
+            return;
+        }
+        $postmeta_value = get_post_meta($post->ID, $this->meta_key, true);
+        if (!$postmeta_value) {
+            return;
+        }
+?>
+        <meta name="<?php echo $this->meta_key ?>" content="<?php echo  $postmeta_value ?>" />
+<?php
+
     }
 
     # TOOD:
