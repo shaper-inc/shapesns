46785115e846a3d73fb05cfc0a73526a29118aa6
eotel <49769982+Eotel@users.noreply.github.com>
2024-07-03 13:11:39 +0900

    🎨 editorconfigに従いtabインデントに修正 + 型や修飾子の追加

--- shapesns/ShapeSns/App.php
+++ shapesns/ShapeSns/App.php
@@ -4,11 +4,11 @@ namespace ShapeSns;
 
 class App extends AppBase
 {
-    protected $admin = null;
-    protected $option = null;
-    protected $meta_key = "記事要約";   // TODO 設定
+	protected mixed $admin = null;
+	protected mixed $option = null;
+	protected mixed $meta_key = "記事要約";   // TODO 設定
 
-	function __construct(array $argument = array())
+	public function __construct(array $argument = [])
 	{
 		parent::__construct($argument);
 		$this->admin = Admin::get_instance();
@@ -16,7 +16,7 @@ class App extends AppBase
 	}
 
 	# 管理画面
-	function action_admin_menu()
+	public function action_admin_menu()
 	{
 		add_options_page(
 			'ShapeSns options',
@@ -27,51 +27,48 @@ class App extends AppBase
 		);
 	}
 
-    function summarize_text($text)
-    {
-        return strip_tags($text);   // 不要文字を抜く
-    }
+	public function summarize_text($text): string
+	{
+		return strip_tags($text);   // 不要文字を抜く
+	}
 
-    function summarize_post($post_id)
-    {
+	public function summarize_post($post_id): void
+	{
+		$wp_post = get_post($post_id);
 
-        $wp_post = get_post($post_id);
+		if ($wp_post) {
+			$content = $wp_post->post_content;
+			$title = $wp_post->post_title;
+			$summary = $this->summarize_text($content);
+			if ($summary) {
+				update_post_meta($post_id, $this->meta_key, $summary);
+			}
+		}
+	}
 
-        if ($wp_post) {
-            $content = $wp_post->post_content;
-            $title = $wp_post->post_title;
-            $summary = $this->summarize_text($content);
-            if ($summary) {
-                update_post_meta($post_id, $this->meta_key, $summary);
-            }
-        }
-    }
+	public function action_wp_enqueue_scripts(): void
+	{
+		# Javascriptを配信
+		# https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+		# https://developer.wordpress.org/reference/functions/plugins_url/
+		wp_enqueue_script('shapesns', plugins_url('/js/shapesns.js', dirname(__FILE__)));
+	}
 
-    function action_wp_enqueue_scripts()
-    {
-        # Javvascriptを配信
-        # https://developer.wordpress.org/reference/functions/wp_enqueue_script/
-        # https://developer.wordpress.org/reference/functions/plugins_url/
-        wp_enqueue_script('shapesns', plugins_url('/js/shapesns.js', dirname(__FILE__)));
-    }
+	public function action_wp_head(): void
+	{
+		# ヘッダーに要約を入れる
+		$post = get_post();
+		if (!$post) {
+			return;
+		}
+		$postmeta_value = get_post_meta($post->ID, $this->meta_key, true);
+		if (!$postmeta_value) {
+			return;
+		}
+		?>
+		<meta name="<?php echo $this->meta_key ?>" content="<?php echo $postmeta_value ?>"/>
+		<?php
+	}
 
-    function action_wp_head()
-    {
-        # ヘッダーに要約を入れる
-        $post = get_post();
-        if (!$post) {
-            return;
-        }
-        $postmeta_value = get_post_meta($post->ID, $this->meta_key, true);
-        if (!$postmeta_value) {
-            return;
-        }
-?>
-        <meta name="<?php echo $this->meta_key ?>" content="<?php echo  $postmeta_value ?>" />
-<?php
-
-    }
-
-    # TOOD:
-    # wordpress hook を実装する
+	# TODO: wordpress hook を実装する
 }
